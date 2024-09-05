52974a2dab0cdf36cdcb96ebe54a07b787b6be3b
Reece Ward <46360824+reecenward@users.noreply.github.com>
2024-09-04 18:40:31 -0700

    google anayltics test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
+import partytown from '@astrojs/partytown'
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), compress()],
+  integrations: [
+    react(),
+     compress(),
+     partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+  }),
+    ],
   devToolbar: {
     enabled: false
   },
@@ -15,3 +23,7 @@ export default defineConfig({
     imageService: true,
   }),
 });
+
+
+
+
