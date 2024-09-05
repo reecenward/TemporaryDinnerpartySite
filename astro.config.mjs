96b6bf111f6478302d16be101b75dda5b511798d
Reece Ward <46360824+reecenward@users.noreply.github.com>
2024-09-05 14:05:27 -0700

    more seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,23 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-     compress(),
-     partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-  }),
-    ],
+  site: 'https://dinnerparty.studio',
+  integrations: [react(), compress(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()],
   devToolbar: {
     enabled: false
   },
   output: "server",
   adapter: vercel({
-    imageService: true,
-  }),
+    imageService: true
+  })
 });
-
-
-
-
